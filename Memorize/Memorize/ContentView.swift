e086d58015d95c5c927513002e242f3454fa0c51
김동욱 <ehddnr73@naver.com>
2022-09-02 09:31:49 +0900

    레이아웃 세팅
    - 서로 다른 이모지 카드 추가
    - add와 remove 버튼 추가

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,11 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚓", "🚑", "🚒", "🚐", "🛻", "🚚"]
+    @State var emojisCount: Int = 6
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
+                    ForEach(emojis[0..<emojisCount], id: \.self) { emoji in
+                        CardView(emoji: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            
+            Spacer()
+            
+            HStack {
+                addButton
+                Spacer()
+                removeButton
+            }
             .padding()
+        }
+    }
+    
+    var addButton: some View {
+        Button(action: {
+            if emojisCount < emojis.count {
+                emojisCount += 1
+            }
+        }) {
+            Image(systemName: "plus.circle")
+                .font(.largeTitle)
+        }
+    }
+    
+    var removeButton: some View {
+        Button(action: {
+            if emojisCount > 0 {
+                emojisCount -= 1
+            }
+        }) {
+            Image(systemName: "minus.circle")
+                .font(.largeTitle)}
     }
 }
+
+
+
+
+struct CardView: View {
+    @State var isFaceUp: Bool = false
+    var emoji: String
+    var shape = RoundedRectangle(cornerRadius: 25)
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                shape
+                    .fill()
+                    .foregroundColor(.white)
+                
+                shape
+                    .strokeBorder(lineWidth: 3)
+                    .foregroundColor(.red)
+                
+                Text(emoji)
+                    .font(.largeTitle)
+            } else {
+                shape
+                    .fill()
+                    .foregroundColor(.red)
+            }
+        }.onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
