e3705c8707ef0d7fd9ac941d37094c22c900e41c
김동욱 <ehddnr73@naver.com>
2022-09-16 00:25:02 +0900

    파이 애니메이션 추가

--- Memorize/Memorize/Cardify.swift
+++ Memorize/Memorize/Cardify.swift
@@ -7,13 +7,22 @@
 
 import SwiftUI
 
-struct Cardify: ViewModifier {
-    var isFaceUp: Bool
+struct Cardify: ViewModifier, Animatable {
+    var rotation: Double
+    
+    var animatableData: Double {
+        get { rotation }
+        set { rotation = newValue }
+    }
+    
+    init(isFaceUp: Bool) {
+        rotation = isFaceUp ? 0 : 180
+    }
     
     func body(content: Content) -> some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-            if isFaceUp {
+            if rotation < 90 {
                 shape
                     .fill()
                     .foregroundColor(.white)
@@ -27,8 +36,9 @@ struct Cardify: ViewModifier {
                     .fill()
                     .foregroundColor(.red)
             }
-            content.opacity(isFaceUp ? 1:0)
+            content.opacity(rotation < 90 ? 1 : 0)
         }
+        .rotation3DEffect(Angle.degrees(rotation), axis: (0, 1, 0))
     }
 }
 
--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -10,28 +10,105 @@ import SwiftUI
 struct EmojiMemoryGameView: View {
     // viewModel의 변화 감지
     @ObservedObject var game: EmojiMemoryGame
+    // Namespace는 일종의 토큰
+    @Namespace private var dealingNamespace
+    
     var body: some View {
-        VStack {
-            card
-            shuffleButton
+        ZStack(alignment: .bottom) {
+            VStack {
+                gameBody
+                
+                HStack {
+                    shuffleButton
+                    Spacer()
+                    restartButton
+                }
+                .padding(.horizontal)
+            }
+            deckBody
         }
     }
     
-    var card: some View {
+    @State private var dealt = Set<Int>()
+    
+    private func deal(_ card: EmojiMemoryGame.Card) {
+        dealt.insert(card.id)
+    }
+    
+    private func isUndealt(_ card: EmojiMemoryGame.Card) -> Bool {
+        !dealt.contains(card.id)
+    }
+    
+    private func zIndex(of card: EmojiMemoryGame.Card) -> Double {
+        -Double(game.cards.firstIndex(where: { $0.id == card.id } ) ?? 0)
+    }
+    
+    var gameBody: some View {
         AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
-            if card.isMatched && !card.isFaceUp {
+            if isUndealt(card) || card.isMatched && !card.isFaceUp {
                 Color.clear
             } else {
                 CardView(card: card)
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
                     .padding(4)
-                    .onTapGesture { game.choose(card) }
+                    .transition(AnyTransition.asymmetric(insertion: .identity, removal: .scale))
+                    .zIndex(zIndex(of: card))
+                    .onTapGesture {
+                        withAnimation {
+                            game.choose(card)
+                        }
+                    }
+            }
+        }
+        .onAppear {
+            game.shuffle()
+        }
+    }
+    
+    var deckBody: some View {
+        ZStack {
+            ForEach(game.cards.filter(isUndealt)) { card in
+                CardView(card: card)
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
+                    .transition(AnyTransition.asymmetric(insertion: .opacity, removal: .identity))
+                    .zIndex(zIndex(of: card))
+            }
+        }
+        // frame을 지정해주지 않으면 가능한 한 가로, 세로의 모든 공간을 차지
+        .frame(width: CardConstants.undealtWidth, height: CardConstants.undealtHeight)
+        .foregroundColor(CardConstants.color)
+        .onTapGesture {
+            withAnimation(.easeInOut(duration: 1)) {
+                for card in game.cards {
+                    withAnimation(dealAnimation(for: card)) {
+                        deal(card)
+                    }
+                }
             }
         }
     }
+    
+    private func dealAnimation(for card: EmojiMemoryGame.Card) -> Animation {
+        var delay: Double = 0.0
+        if let index = game.cards.firstIndex(where: { $0.id == card.id }) {
+            delay = Double(index) * (CardConstants.totalDealDuration / Double(game.cards.count))
+        }
+        return Animation.easeInOut(duration: CardConstants.dealDuration).delay(delay)
+    }
+    
     var shuffleButton: some View {
         Button("Shuffle") {
             withAnimation {
                 game.shuffle()
+            }
+        }
+    }
+    
+    var restartButton: some View {
+        Button("Restart") {
+            withAnimation {
+                game.restart()
+                dealt = []
             }
         }
     }
@@ -40,13 +117,27 @@ struct EmojiMemoryGameView: View {
 struct CardView: View {
     let card: EmojiMemoryGame.Card
     
+    @State private var animatedBonusRemaining: Double = 0
+    
     var body: some View {
         // GeometryReader로 CardView의 크기를 계산
         GeometryReader { geometry in
             ZStack {
-                    Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 120-90))
+                Group {
+                    if card.isConsumingBonusTime {
+                        Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: (1-animatedBonusRemaining)*360-90))
+                            .onAppear {
+                                animatedBonusRemaining = card.bonusRemaning
+                                withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+                                    animatedBonusRemaining = 0
+                                }
+                            }
+                    } else {
+                        Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: (1-card.bonusRemaning)*360-90))
+                    }
+                }
                         .padding(5)
-                        .foregroundColor(.red)
+                        .foregroundColor(CardConstants.color)
                         .opacity(0.5)
 
                     Text(card.content)
@@ -70,10 +161,18 @@ private struct DrawingConstants {
     static let fontSize: CGFloat = 32
 }
 
+private struct CardConstants {
+    static let color = Color.red
+    static let aspectRatio: CGFloat = 2/3
+    static let undealtWidth: CGFloat = undealtHeight * aspectRatio
+    static let undealtHeight: CGFloat = 90
+    static let dealDuration: Double = 0.5
+    static let totalDealDuration: Double = 3
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         let game = EmojiMemoryGame()
-        game.choose(game.cards.first!)
         return EmojiMemoryGameView(game: game)
     }
 }
